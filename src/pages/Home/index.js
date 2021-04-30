7b1d42536451a9f359d05a393488d2a2f4a68ebf
lpeil <luan@peil.dev>
2021-04-30 17:11:29 -0300

    feat: create home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const HomePage = () => (
-  <h1>Home</h1>
-);
+import { Grid, Button } from '@material-ui/core';
+
+import { removeUserSession } from '../../store/modules/auth/actions';
+
+const HomePage = () => {
+  const user = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const logout = () => {
+    dispatch(removeUserSession());
+  };
+
+  return (
+    <Grid container justify="center" style={{ marginTop: '50px' }}>
+      <Grid container item direction="column" xs={10}>
+        <Grid item>
+          <h1 className="font font-size-5 color-black">
+            Olá,
+            {' '}
+            {user.name}
+            .
+          </h1>
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            onClick={logout}
+          >
+            Sair
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default HomePage;
